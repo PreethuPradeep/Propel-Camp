21d32d254930cc1fe94bf3bd10e49dfbb984c714
PreethuPradeep <nocturnalsurfer@proton.me>
2025-06-06 14:36:40 +0530

    Update bankATM.py

--- bank-atm-system/bankATM.py
+++ bank-atm-system/bankATM.py
@@ -1,273 +1,221 @@
-#The username and pass that validates administrator login
-username="admin"
-password="1234"
-#Admin_login takes us to the login page
-#here the inputed values are compared to given data for validation
-#if correct, then login suucessful
-#if not login invalid
+import random
+
+username = "admin"
+password = "1234"
+
+customers = {}
+
 def admin_login():
-    uname=input("Username: ")
-    pwd=input("Password: ")
-    if uname==username and pwd==password:
-        print("\tLogin Successfull!!!")
+    uname = input("Username: ")
+    pwd = input("Password: ")
+    if uname == username and pwd == password:
+        print("\tLogin Successful!!!")
         print("\tWelcome Administrator!!!")
         return True
     else:
         print("\tInvalid Credentials!")
-        return
-
-#this the dictionary containing all data regarding the customers of bank
-#while begining the code, the customer data is empty.
-#So the customer dictionary ought be filled first using add_customer().
-#Then only can we access customer login.
-# customer login can only be done inside admin menu using the confidential data like account noa dn atm pin.  
-customers={}
-#Add customers()
+        return False
 
 def add_customer():
-    import random
-    account_no=random.randint(100000000,999999999)# account number is randomly generated 9 digit number by the computer 
-    if account_no in customers:#verifying if account numvber exists or not
+    account_no = random.randint(100000000, 999999999)
+    if account_no in customers:
         print("\tCustomer already exists!!!")
-    else:#if it doesnt
-        print("\n \tAccount Number: ",account_no)#values are inputed
-        name=input("Enter the Customer Name: ").title()#.title() makes the first letters capital for each word
-        if len(name)>30:#length of the name cannot be bigger than 30
-                print("Customer Name should not be more than 30 characters!!!")
-                return
-        type=input("Enter if Account Type is Savings or Current: ").upper()#.upper makes so that anything entered here is in uppercase
-        if type!="SAVINGS" and type!="CURRENT":#to make sure only SAVINGS or CURRENT are inputed
-            return
-        try:#only integers are in balance
-            balance=int(input("Enter the account balance: "))
-        except ValueError:
-            print("Enter only numbers!")
-            return
-        try:#only integers are in min_bal
-            min_bal=int(input("Enter the minimum balance of this account: "))
-        except ValueError:
-            print("Enter only numbers!")
-            return
-        if balance<min_bal:#account balance must always be greater than minimum balance
-            print("Account Balance must always be greater than minimum balance!")
-            return
-        try:#mobile nu must always be numerical
-            mobile_no=int(input("Enter your mobile no: "))
-            if mobile_no<1000000000 or mobile_no>9999999999:#mobile number must always be 10 digit number
-                print("A mobile number contains only 10 digits! Try Again!")
-                return
-        except ValueError:
-            print("Enter only numbers!")
-            return
-        email_id=input("Enter the email id: ")
-        pan_number=input("Enter your pan number").upper()
-        import random
-        ATM_PIN=random.randint(1000,9999)#randomly generated 4 digit number as pin
-        customers[account_no]={"Account no: ":account_no,"Customer Name: ":name,"Account type: ":type,"Account Balance: ":balance,"Minimum Balance: ":min_bal,"Customer Mobile No: ":mobile_no,"Customer Email Id: ":email_id,"Customer Pan number: ":pan_number,"ATM PIN: ":ATM_PIN}
-        print("\tCustomer Details Entered Sucessfully!!!")
-
-def update_customer():#to update customer details
-    try:#account number must be numerical
-        account_no=int(input("Enter the Account Number of the Customer whose details you want to update: "))
-    except ValueError:
-        print("Invalid Account Number. Must be a number.")
         return
-    if account_no in customers:#if the account no exists then you can update
-        try:
-            new_account_no=input(f"Update Customer Account No: [{customers[account_no]['Account no: ']}]") or customers[account_no]['Account no: ']
-        except ValueError:
-            print("Enter only numbers!")
-            return
-        try:
-            mobile_no=input(f"Update Customer Mobile No: [{customers[account_no]['Customer Mobile No: ']}]") or customers[account_no]['Customer Mobile No: ']
-        except ValueError:
-            print("Enter only numbers!")
-            return
-        if mobile_no<1000000000 or mobile_no>9999999999:#mobile number must always be 10 digit number
+    print("\n\tAccount Number: ", account_no)
+    name = input("Enter the Customer Name: ").title()
+    if len(name) > 30:
+        print("Customer Name should not be more than 30 characters!!!")
+        return
+    acc_type = input("Enter if Account Type is Savings or Current: ").upper()
+    if acc_type not in ("SAVINGS", "CURRENT"):
+        print("Invalid Account Type! Must be SAVINGS or CURRENT.")
+        return
+    try:
+        balance = int(input("Enter the account balance: "))
+    except ValueError:
+        print("Enter only numbers!")
+        return
+    try:
+        min_bal = int(input("Enter the minimum balance of this account: "))
+    except ValueError:
+        print("Enter only numbers!")
+        return
+    if balance < min_bal:
+        print("Account Balance must always be greater than minimum balance!")
+        return
+    try:
+        mobile_no = int(input("Enter your mobile no: "))
+        if mobile_no < 1000000000 or mobile_no > 9999999999:
             print("A mobile number contains only 10 digits! Try Again!")
             return
-        email_id=input(f"Update Customer Email Id: [{customers[account_no]['Customer Email Id: ']}]") or customers[account_no]['Customer Email Id: ']
-        customers[account_no]={"Account No: ":new_account_no,"Customer Mobile No: ":mobile_no,"Customer Email Id: ":email_id}
-        print("\tAccount details updated Successfully!!!")
+    except ValueError:
+        print("Enter only numbers!")
+        return
+    email_id = input("Enter the email id: ")
+    pan_number = input("Enter your PAN number: ").upper()
+    ATM_PIN = random.randint(1000, 9999)
+    customers[account_no] = {
+        "Account No: ": account_no,
+        "Customer Name: ": name,
+        "Account Type: ": acc_type,
+        "Account Balance: ": balance,
+        "Minimum Balance: ": min_bal,
+        "Customer Mobile No: ": mobile_no,
+        "Customer Email Id: ": email_id,
+        "Customer Pan Number: ": pan_number,
+        "ATM PIN: ": ATM_PIN
+    }
+    print(f"\tCustomer Details Entered Successfully! Your ATM PIN is {ATM_PIN}")
 
-    else:
-        print("No such account exists in the database!")
-
-def delete_customer():#to remove a customer
+def update_customer():
     try:
-        account_no=int(input("Enter the account number of the customer you want to remove: "))
+        account_no = int(input("Enter the Account Number of the Customer to update: "))
     except ValueError:
         print("Invalid Account Number. Must be a number.")
         return
     if account_no in customers:
-        del customers[account_no]
-        print("\tCustomer Data Deleted Successfully!")
+        customer = customers[account_no]
+        new_mobile = input(f"Update Customer Mobile No [{customer['Customer Mobile No: ']}]: ") or str(customer['Customer Mobile No: '])
+        try:
+            new_mobile_int = int(new_mobile)
+            if new_mobile_int < 1000000000 or new_mobile_int > 9999999999:
+                print("Mobile number must be 10 digits.")
+                return
+        except ValueError:
+            print("Enter only numbers for mobile no.")
+            return
+        new_email = input(f"Update Customer Email Id [{customer['Customer Email Id: ']}]: ") or customer['Customer Email Id: ']
+        # Update fields
+        customer['Customer Mobile No: '] = new_mobile_int
+        customer['Customer Email Id: '] = new_email
+        print("\tAccount details updated Successfully!!!")
     else:
-        print("\tNo Account that belongs to this ID exists in this Database!")
+        print("No such account exists in the database!")
 
-def view_all_customers():#prints out all the account data stored in the bank database
-    if not customers:
-        print("\tNo Accounts opened in the Bank!!!")
-    else:
-        for account_no, info in customers.items():#the loop makes sure that all account details are shown
-            print(f"\nAccount Number: ,{info['Account No: ']}")
-            print(f"    Customer Name: ,{info['Customer Name: ']}")
-            print(f"    Account Type: ,{info['Account Type: ']}")
-            print(f"    Account Balance: : {info['Account Balance: ']}")
-            print(f"    Minimum Balance: {info['Minimum Balance: ']}")
-            print(f"    Customer Mobile No: {info['Customer Mobile No: ']}")
-            print(f"    Customer Email Id: {info['Customer Email Id: ']}")
-            print(f"    ATM PIN: {info['ATM PIN: ']}")
-
-def find_customer():#to find a specific customer and their details
-    try:#you need their account no
-        account_no=int(input("Enter the Account Number of the customer you want to find: "))
-    except ValueError:#makes sure its a number
-        print("Invalid Account Number. Must be a number.")
-        return
-    if account_no in customers:#check if the number is in the dictionary
-        print("Customer Found!")
-        print("\n   Account No: ",account_no)
-        print("    Customer Name: ",customers[account_no]['Customer Name: '])
-        print("    Account Type: ",customers[account_no]['Account Type: '])
-        print("    Account Balance: ",customers[account_no]['Account Balance: '])
-        print("    Minimum Balance: ",customers[account_no]['Minimum Balance: '])
-        print("    Customer Mobile No: ",customers[account_no]['Customer Mobile No: '])
-        print("    Customer Email Id: ",customers[account_no]['Customer Email Id: '])
-        print("    ATM PIN: ",customers[account_no]['ATM PIN: '])
-    else:
-        print("Account number not found..!!!")
-
-def customer_login():#here we are entering as a customer using their account no and atm pin. the database should be started and customers must be added before customer can login
-    account_no=int(input("Enter the Account No:"))#account no instead of username
-    pwd=int(input("Enter your ATM PIN: "))#atm pin instead of password
-    if account_no in customers:#if account number in dictionary
-        if pwd==customers[account_no]['ATM_PIN']:#and the pin entered is same as the pin in the account number dictionary
-            print("\tLogin Successfull!!!")
-            print("\tWelcome Customer!!!")
-            return customer_menu()
-    else:
-        print("\tInvalid Credentials!")
-
-
-def deposit_money():#after customer login only you can deposit money
-    try:#only numbers should be entered
-        account_no=int(input("Enter your account number: "))
+def deposit_money():
+    try:
+        account_no = int(input("Enter your account number: "))
         if account_no not in customers:
+            print("Account not found.")
             return
-        deposit=int(input("Enter the amount to be deposited: ")) 
+        deposit = int(input("Enter the amount to be deposited: "))
     except ValueError:
-        print("Enter numerical vlue only!")
+        print("Enter numerical value only!")
         return
-    if deposit>=50000:#If Amount is greater than 50k then ask the customer to enter his/her PAN Card number
-        pan_no=input("Enter pan number")
-        if pan_no==customers[account_no]['Pan Number']:
-            customers[account_no]["Account Balance: "]+=deposit
-            print(f"Your Account Balnce is now {customers[account_no]["Account Balance: "]}")
-        else:
+    if deposit >= 50000:
+        pan_no = input("Enter PAN number: ").upper()
+        if pan_no != customers[account_no]['Customer Pan Number: ']:
+            print("PAN number mismatch! Deposit cancelled.")
             return
-    else:
-        customers[account_no]["Account Balance: "]+=deposit
-        print(f"Your Account Balnce is now {[customers[account_no]["Account Balance: "]]}")
+    customers[account_no]["Account Balance: "] += deposit
+    print(f"Your Account Balance is now {customers[account_no]['Account Balance: ']}")
     print("\tMoney successfully deposited!")
 
-def withdraw_money():#after customer login only you can withdraw money
+def withdraw_money():
     try:
-        account_no=int(input("Enter your account number: "))
+        account_no = int(input("Enter your account number: "))
         if account_no not in customers:
+            print("Account not found.")
             return
-        withdraw=int(input("Enter the amount to be withdraw: "))
+        withdraw = int(input("Enter the amount to be withdrawn: "))
     except ValueError:
-        print("Enter numerical vlue only!")
+        print("Enter numerical value only!")
         return
-    if withdraw>=50000:#If Amount is greater than 50k then ask the customer to enter his/her PAN Card number
-        pan_no=input("Enter pan number")
-        if pan_no==customers[account_no]['Pan Number']:
-            customers[account_no]["Account Balance: "]+=withdraw
-            print(f"Your Account Balnce is now {customers[account_no]["Account Balance: "]}")
-        else:
+    if withdraw >= 50000:
+        pan_no = input("Enter PAN number: ").upper()
+        if pan_no != customers[account_no]['Customer Pan Number: ']:
+            print("PAN number mismatch! Withdrawal cancelled.")
             return
+    if customers[account_no]["Account Balance: "] - withdraw >= customers[account_no]["Minimum Balance: "]:
+        customers[account_no]["Account Balance: "] -= withdraw
+        print(f"Your Account Balance is now {customers[account_no]['Account Balance: ']}")
+        print("\tMoney successfully withdrawn!")
     else:
-        customers[account_no]["Account Balance: "]-=withdraw
-        if customers[account_no]["Account Balance: "]>=customers[account_no]["Minimum Balance: "]:#the account balance must remain greater than
-            print(f"Your Account Balnce is now {[customers[account_no]["Account Balance: "]]}")
-            print("\tMoney successfully withdrawn!")
-        else:
-            customers[account_no]["Account Balance: "]+=withdraw
-            print("“Insufficient funds..!!!")
+        print("Insufficient funds..!!!")
 
-
-def show_balance():#show balance
+def customer_login():
     try:
-        account_no=int(input("Enter your account number: "))#make sure account in dictionary
-        if account_no not in customers:
-            return
+        account_no = int(input("Enter the Account No: "))
+        pwd = int(input("Enter your ATM PIN: "))
     except ValueError:
-        print("Enter Numerical Values Only!")
-    print(f"Your Account Balnce is now {[customers[account_no]["Account Balance: "]]}")
+        print("Account number and PIN must be numeric.")
+        return
+    if account_no in customers:
+        if pwd == customers[account_no]['ATM PIN: ']:
+            print("\tLogin Successful!!!")
+            print("\tWelcome Customer!!!")
+            customer_menu(account_no)
+        else:
+            print("\tInvalid PIN!")
+    else:
+        print("\tInvalid Account Number!")
 
+def show_balance(account_no):
+    print(f"Your Account Balance is now {customers[account_no]['Account Balance: ']}")
 
-def transfer_money():
+def transfer_money(account_no_from):
     try:
-        transfer=int(input("Enter the amount to be transferred"))
-        account_no1=int(input("Transferred from: "))
-        account_no2=int(input("Transferred to: "))
+        amount = int(input("Enter the amount to be transferred: "))
+        account_no_to = int(input("Transferred to (Account No): "))
     except ValueError:
         print("Numerical values only!")
         return
-    withdraw_money(account_no1)
-    deposit_money(account_no2)
-    show_balance(account_no1)
-    print("“ Transferred Amount Successfully”")
+    if account_no_to not in customers:
+        print("Receiver account not found!")
+        return
+    if customers[account_no_from]["Account Balance: "] - amount < customers[account_no_from]["Minimum Balance: "]:
+        print("Insufficient funds for transfer.")
+        return
+    customers[account_no_from]["Account Balance: "] -= amount
+    customers[account_no_to]["Account Balance: "] += amount
+    print(f"Transferred {amount} from account {account_no_from} to {account_no_to} successfully.")
+    show_balance(account_no_from)
 
-
-def admin_menu():
+def customer_menu(account_no):
     while True:
-        print("\n1. Adding a New Customer")
-        print("2. Updating Customer Details")
-        print("3. Deleting a Customer")
-        print("4. Display the list of all Customers in the Bank")
-        print("5. Display Customer Details of a specific Customer")
-        print("6. Go to customer login")
-        print("7. Exit")
-
-        choice=input("Enter your Choice")
-        if choice=="1":
-            add_customer()
-        elif choice=="2":
-            update_customer()
-        elif choice=="3":
-            delete_customer()
-        elif choice=="4": 
-            view_all_customers()
-        elif choice=="5": 
-            find_customer()
-        elif choice=="6": 
-            customer_login()
-        elif choice=="7": 
-            print("\tExiting the system...")
+        print("\n1. Deposit Money")
+        print("2. Withdraw Money")
+        print("3. Show Balance")
+        print("4. Transfer Money")
+        print("5. Logout")
+        choice = input("Enter your Choice: ")
+        if choice == "1":
+            deposit_money()
+        elif choice == "2":
+            withdraw_money()
+        elif choice == "3":
+            show_balance(account_no)
+        elif choice == "4":
+            transfer_money(account_no)
+        elif choice == "5":
+            print("\tLogging out...")
             break
         else:
             print("\tInvalid Choice. Please Try again.")
 
-def customer_menu():
+def admin_menu():
     while True:
-        print("\n1. Depositing Money into Customer Account")
-        print("2. Withdraw Money from Customer Account")
-        print("3. Show Balance of a Customer")
-        print("4. Transferring Money from One Account to Another account")
-        print("5. exit")
-
-        choice=input("Enter your Choice")
-        if choice=="1":
-            deposit_money()
-        elif choice=="2":
-            withdraw_money()
-        elif choice=="3":
-            show_balance()
-        elif choice=="4": 
-            transfer_money()
-        elif choice=="5": 
+        print("\n1. Add Customer")
+        print("2. Update Customer Details")
+        print("3. Delete Customer")
+        print("4. View All Customers")
+        print("5. Find Customer by Account Number")
+        print("6. Customer Login")
+        print("7. Exit")
+        choice = input("Enter your Choice: ")
+        if choice == "1":
+            add_customer()
+        elif choice == "2":
+            update_customer()
+        elif choice == "3":
+            delete_customer()
+        elif choice == "4":
+            view_all_customers()
+        elif choice == "5":
+            find_customer()
+        elif choice == "6":
+            customer_login()
+        elif choice == "7":
             print("\tExiting the system...")
             break
         else:
@@ -279,4 +227,6 @@ def main():
     else:
         print("Try Again!")
         return
+
 main()
+
